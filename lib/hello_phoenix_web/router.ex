e1e4b0d6a3bbc4cfff26ca81ffd30d3b246eb170
waseem-medhat <wipdev.tech@gmail.com>
2024-05-25 22:54:59 +0300

    Add param-dependent plug for locale

--- lib/hello_phoenix_web/router.ex
+++ lib/hello_phoenix_web/router.ex
@@ -8,6 +8,7 @@ defmodule HelloPhoenixWeb.Router do
     plug :put_root_layout, html: {HelloPhoenixWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug HelloPhoenixWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
