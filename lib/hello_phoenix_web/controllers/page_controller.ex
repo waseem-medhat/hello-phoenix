89eeaaec47a1488f3e265825141eb88ef61ffeb5
waseem-medhat <wipdev.tech@gmail.com>
2024-05-26 10:39:11 +0300

    Move locale plug from router to controller

--- lib/hello_phoenix_web/controllers/page_controller.ex
+++ lib/hello_phoenix_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule HelloPhoenixWeb.PageController do
   use HelloPhoenixWeb, :controller
 
+  plug HelloPhoenixWeb.Plugs.Locale, "en" when action in [:home]
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
